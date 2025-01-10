18436bf9194476bc5cefb9baa06b7dcd9cb121db
Aashish Katiyar <aashishkatiyar20@gmail.com>
2025-01-10 18:06:15 +0530

    temp

---
+++ Aashish_SwiftUI-Weather/Aashish_SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aashish_SwiftUI_WeatherApp.swift
+//  Aashish_SwiftUI-Weather
+//
+//  Created by Aashish Katiyar on 10/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Aashish_SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
